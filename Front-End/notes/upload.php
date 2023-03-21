6cd057ab47da198c831abdf9f875884266f92e25
Salman Moghul <120734318+Sallu9007@users.noreply.github.com>
2023-03-20 23:13:22 -0700

    Add files via upload

--- Front-End/notes/upload.php
+++ Front-End/notes/upload.php
@@ -1,12 +1,19 @@
 <?php
-        $FileName=$_FILES['notesfile']['name'];
-	$TmpName=$_FILES['notesfile']['tmp_name'];
+        $fileName=$_FILES['notesfile']['name'];
+	$tmpName=$_FILES['notesfile']['tmp_name'];
+        $branch = $_POST['branch'];
+        $subject = $_POST['subject'];
+        $module = $_POST['module'];
 
-
-        move_uploaded_file ($TmpName, $FileName);
-        echo("File Uploaded Successfuly");
-        // header("Location: http://localhost:8000/HTML/upload_notes.html");
-        header("Location: ../HTML/upload_notes.html");
+        $filePath =  $branch . '/' . $subject . '/' . $module;
+        if (!file_exists($filePath)) {
+        mkdir($filePath, 0777, true);
+        }
+        $fileDirectory = $filePath . '/' . $fileName;
+        echo $fileDirectory;
+        move_uploaded_file ($tmpName, $fileDirectory);
+        header("Location: http://localhost:8000/HTML/upload_notes.html");
+        // header("Location: ../HTML/upload_notes.html");
         // header("Location: ../HTML/login.html");
         // echo '<script>alert("Welcome to Geeks for Geeks")</script>';
               
